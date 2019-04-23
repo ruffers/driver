fc8ed8023ff07b872732aae0ccf04958af90535d
huangxiaolong <huangxiaolong03@baidu.com>
2019-04-23 18:18:32 +0800

    登录界面UI

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,12 +1,6 @@
-import React, {PureComponent} from 'react';
+import React, { PureComponent } from 'react';
 import { Actions } from 'react-native-router-flux';
-import {
-    StyleSheet,
-    Text,
-    View,
-    StatusBar,
-    TouchableOpacity
-} from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TouchableOpacity, TextInput } from 'react-native';
 import LoginStyle from './LoginStyle';
 
 class Login extends PureComponent {
@@ -21,9 +15,26 @@ class Login extends PureComponent {
                 <View style={LoginStyle.loginSlider}>
                     <View style={LoginStyle.loginItem}>
                         <View style={LoginStyle.loginContent}>
-                            <TouchableOpacity onPress={this.login}>
-                                <Text style={LoginStyle.welcome}>登录</Text>
-                            </TouchableOpacity>
+                            <View style={LoginStyle.loginTitleWrap}>
+                                <Text style={LoginStyle.loginTitle}>登录</Text>
+                            </View>
+                            <View style={LoginStyle.inputs}>
+                                <View style={LoginStyle.inputItem}>
+                                    <Text style={LoginStyle.label}>账号</Text>
+                                    <TextInput style={LoginStyle.userName} selectionColor="#000" autoFocus={false} value=""/>
+                                </View>
+                                <View style={LoginStyle.inputItem}>
+                                    <Text style={LoginStyle.label}>密码</Text>
+                                    <TextInput style={LoginStyle.userName} selectionColor="#000" autoFocus={false} value=""/>
+                                </View>
+                            </View>
+                            <View style={LoginStyle.loginBtnWrap}>
+                                <View style={LoginStyle.loginBtn}>
+                                    <TouchableOpacity onPress={this.login}>
+                                        <Text style={LoginStyle.loginBtnTxt}>登录</Text>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
                         </View>
                     </View>
                 </View>
--- src/Pages/Login/LoginStyle.js
+++ src/Pages/Login/LoginStyle.js
@@ -1,6 +1,4 @@
-import {
-    StyleSheet
-} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     loginWrap: {
@@ -16,12 +14,49 @@ export default StyleSheet.create({
         justifyContent: 'center'
     },
     loginContent: {
-
+        width: '100%',
+        paddingHorizontal: px2dp(40)
     },
-    welcome: {
-        fontSize: 30,
+    loginTitleWrap: {
+        marginBottom: px2dp(88)
+    },
+    loginTitle: {
+        fontSize: px2dp(50),
         textAlign: 'center',
-        margin: 10,
         color: '#000'
+    },
+    inputs: {
+        marginBottom: px2dp(220)
+    },
+    inputItem: {
+        borderBottomWidth: px2dp(1),
+        borderColor: '#c9c9c9',
+        paddingVertical: px2dp(26),
+        flexDirection: 'row'
+    },
+    label: {
+        fontSize: px2dp(35),
+        paddingRight: px2dp(135)
+    },
+    userName: {
+        flex: 1,
+        fontSize: px2dp(35)
+    },
+    loginBtnWrap: {
+        flexDirection: 'row'
+    },
+    loginBtn: {
+        flex: 1,
+        height: px2dp(94),
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#51aa38',
+        borderRadius: px2dp(10),
+        borderWidth: px2dp(1),
+        borderColor: '#499932'
+    },
+    loginBtnTxt: {
+        fontSize: px2dp(35),
+        color: '#fff'
     }
 });
